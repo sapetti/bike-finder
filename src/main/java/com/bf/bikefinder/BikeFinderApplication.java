96ef6035a0165db3e26d601da71b2a785326e545
cesar <gsapetti@hotmail.com>
2018-10-24 19:41:02 +0200

    Initial upload

---
+++ src/main/java/com/bf/bikefinder/BikeFinderApplication.java
@@ -0,0 +1,12 @@
+package com.bf.bikefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeFinderApplication.class, args);
+	}
+}
