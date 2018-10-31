e8747d57eabfcd0efbe6d94037042f6cf0549377
sapetti <gsapetti@hotmail.com>
2018-10-31 15:24:32 +0100

    DataLoader Java8 (alpha version)

--- src/main/java/com/bf/bikefinder/dataloaders/BikeDataLoader.java
+++ src/main/java/com/bf/bikefinder/dataloaders/BikeDataLoader.java
@@ -4,7 +4,13 @@ import java.io.IOException;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+import io.vavr.control.Try;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -12,7 +18,6 @@ import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -58,6 +63,7 @@ public class BikeDataLoader {
 		try {
 			Document doc = Jsoup.connect(url).get();
 			Elements divs = doc.select("div.product");
+
 			for (Element div : divs) {			
 
 				Element link = div.select("a.image_container").first();
@@ -93,12 +99,58 @@ public class BikeDataLoader {
 				}
 			}
 		} catch (ParseException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			LOGGER.error("Unable to parse BH", e);
 		}
 	}
+
+	private static Consumer<Tuple2<Bike, Element>> setAttrInEntity(BiConsumer<Bike, String> setBikeValue, String selector) {
+    	return tuple -> setBikeValue.accept(tuple._1, tuple._2.attr(selector));
+	}
+
+	private static Double parseDouble(String num) throws ParseException {
+		NumberFormat nf = NumberFormat.getInstance(Locale.getDefault());
+		return nf.parse(num).doubleValue();
+	}
+
+	private void loadBikesBHVavr() {
+		String url_en = "https://www.bhbikes.com/en_GB/bikes/";
+		String url = "https://www.bhbikes.com/es_ES/bicicletas";
+
+		Maker maker = Optional.ofNullable(makerRepository.findByName("BH"))
+			.orElse(makerRepository.save(new Maker("BH")));
+
+
+		Try.of(() -> Jsoup.connect(url).get())
+		.getOrElse(() -> Document.createShell(""))
+		.select("div.product")
+		.stream()
+		.filter(div -> !"".equals(div.attr("title"))) // if(!model.equals(""))
+		.map(div -> {
+			String model = div.attr("title");
+			Bike bike = Optional.ofNullable(bikeRepository.findByName(model))
+					.orElseGet(() -> new Bike(model));
+			return Tuple.of(bike, div);
+		})
+		.peek(setAttrInEntity(Bike::setCategory, "data-seccion_web"))
+		.peek(tuple -> tuple._1.setMakerId(maker.getId()))
+		.peek(tuple -> tuple._1.setUrlDetails(tuple._2.select("a.image_container").first().attr("abs:href")))
+		.peek(tuple -> {
+			String priceStr = tuple._2.attr("data-precio").replace(".", ",");
+			Try.of(() -> parseDouble(priceStr))
+					.onSuccess(tuple._1::setPrice);
+		})
+		.peek(tuple -> {
+			Element link = tuple._2.select("a.image_container").first();
+			tuple._1.setUrlDetails(link.attr("abs:href"));
+			Optional.of(link.select("img").first())
+				.ifPresent(element -> tuple._1.setUrlImage(element.attr("src")));
+		})
+		.peek(tuple -> tuple._1.setYear(2018))
+		.forEach(tuple -> bikeRepository.save(tuple._1));
+	}
+
 	private static void print(String msg, Object... args) {
-		System.out.println(String.format(msg, args));
+		LOGGER.debug(String.format(msg, args));
 	}
 
 }
